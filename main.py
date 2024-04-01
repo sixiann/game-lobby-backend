a8bd16584604409594619b2029e702dd45ebf616
Lexa Huang <sixian.duidui@gmail.com>
2024-04-01 02:25:59 -0400

    reformat

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import requests
 import threading
 import time
 
-server_url = 'http://localhost:5000'
+server_url = "http://localhost:5000"
 
-#function to run each individual socketio action 
+
+# function to run each individual socketio action
 def perform_socketio_action(client, action, data, server_url):
     # Event handler for establishing connection
     @client.event
@@ -19,12 +20,12 @@ def perform_socketio_action(client, action, data, server_url):
     @client.event
     def message(response_data):
         # Check for errors in the response and print if present
-        if 'error' in response_data:
-            print("Error:", response_data['error'])
+        if "error" in response_data:
+            print("Error:", response_data["error"])
         # Otherwise, print the notification received
-        elif 'message' in response_data:
+        elif "message" in response_data:
             print(f"Notif received by {data['player_id']}: {response_data['message']}")
-    
+
     # Connect to the server
     client.connect(server_url)
 
@@ -36,9 +37,9 @@ def perform_socketio_action(client, action, data, server_url):
         print("Script interrupted by user.")
 
 
-#run test cases 
-def run_tests(actions): 
-    clients = [] # Store client instances to disconnect later
+# run test cases
+def run_tests(actions):
+    clients = []  # Store client instances to disconnect later
 
     # Iterate over each action to be tested
     for action, data, url in actions:
@@ -46,96 +47,181 @@ def run_tests(actions):
         client = socketio.Client()
         clients.append(client)
         # Create a new thread for each client to perform its action, start the thread
-        thread = threading.Thread(target=perform_socketio_action, args=(client, action, data, url))
+        thread = threading.Thread(
+            target=perform_socketio_action, args=(client, action, data, url)
+        )
         thread.start()
-        #pause between actions for clarity
-        time.sleep(2) 
-    
+        # pause between actions for clarity
+        time.sleep(2)
+
     # After all actions are completed, disconnect each client
     for client in clients:
         client.disconnect()
 
+
 if __name__ == "__main__":
 
-    #1. Testing valid create, join, leave, and reaching max players
-    print("\n" + " Testing valid create, join, leave, and reach max players ".center(80, '-'))
+    # 1. Testing valid create, join, leave, and reaching max players
+    print(
+        "\n"
+        + " Testing valid create, join, leave, and reach max players ".center(80, "-")
+    )
     actions = [
-        ('create_lobby', {'player_id': '1', 'lobby_details': 'my first lobby'}, server_url),          #player 1 creates lobby 1
-        ('join_lobby', {'player_id': '2', 'lobby_id': '1'}, server_url),                              #player 2 joins lobby
-        ('join_lobby', {'player_id': '3', 'lobby_id': '1'}, server_url),                              #player 3 joins lobby
-        ('leave_lobby', {'player_id': '3', 'lobby_id': '1'}, server_url),                             #player 3 leaves lobby
-        ('join_lobby', {'player_id': '4', 'lobby_id': '1'}, server_url),                              #player 4 joins lobby
-        ('join_lobby', {'player_id': '5', 'lobby_id': '1'}, server_url),                              #player 5 joins lobby
-        ('join_lobby', {'player_id': '6', 'lobby_id': '1'}, server_url),                              #player 6 joins lobby, reached max players
-                                                                                                      #lobby 1 is deleted 
+        (
+            "create_lobby",
+            {"player_id": "1", "lobby_details": "my first lobby"},
+            server_url,
+        ),  # player 1 creates lobby 1
+        (
+            "join_lobby",
+            {"player_id": "2", "lobby_id": "1"},
+            server_url,
+        ),  # player 2 joins lobby
+        (
+            "join_lobby",
+            {"player_id": "3", "lobby_id": "1"},
+            server_url,
+        ),  # player 3 joins lobby
+        (
+            "leave_lobby",
+            {"player_id": "3", "lobby_id": "1"},
+            server_url,
+        ),  # player 3 leaves lobby
+        (
+            "join_lobby",
+            {"player_id": "4", "lobby_id": "1"},
+            server_url,
+        ),  # player 4 joins lobby
+        (
+            "join_lobby",
+            {"player_id": "5", "lobby_id": "1"},
+            server_url,
+        ),  # player 5 joins lobby
+        (
+            "join_lobby",
+            {"player_id": "6", "lobby_id": "1"},
+            server_url,
+        ),  # player 6 joins lobby, reached max players
+        # lobby 1 is deleted
     ]
     run_tests(actions)
-    print("\n" + " Testing valid cases complete ".center(80, '-') + "\n")
+    print("\n" + " Testing valid cases complete ".center(80, "-") + "\n")
 
 
 
-    #2. Create 2 lobbies for further testing, and test get_lobbies 
-    print("\n" + " Testing get all lobbies ".center(80, '-'))
+
+    # 2. Create 2 lobbies for further testing, and test get_lobbies
+    print("\n" + " Testing get all lobbies ".center(80, "-"))
     create_actions = [
-        ('create_lobby', {'player_id': '2', 'lobby_details': 'my first lobby'}, server_url),                            #player 2 creates lobby 2
-        ('create_lobby', {'player_id': '3', 'lobby_details': 'another lobby'}, server_url),                             #player 3 creates lobby 3   
+        (
+            "create_lobby",
+            {"player_id": "2", "lobby_details": "my first lobby"},
+            server_url,
+        ),  # player 2 creates lobby 2
+        (
+            "create_lobby",
+            {"player_id": "3", "lobby_details": "another lobby"},
+            server_url,
+        ),  # player 3 creates lobby 3
     ]
     run_tests(create_actions)
     response = requests.get(f"{server_url}/get_lobbies")
     print("\nResponse from /get_lobbies:")
     print(response.status_code, response.json())
-    print("\n" + " Testing get all lobbies complete ".center(80, '-'))
+    print("\n" + " Testing get all lobbies complete ".center(80, "-"))
 
 
-    #3. Testing invalid create lobbies
-    print("\n" + " Testing invalid create lobby cases".center(80, '-'))
+
+
+    # 3. Testing invalid create lobbies
+    print("\n" + " Testing invalid create lobby cases".center(80, "-"))
     invalid_create_actions = [
-        ('create_lobby', {'lobby_details': 'Example Lobby Details'}, server_url),                                           # missing player_id 
-        ('create_lobby', {'player_id': '1'}, server_url),                                                                    # missing lobby_details
-        ('create_lobby', {'player_id': '100', 'lobby_details': 'Example Lobby Details'}, server_url),                         # invalid player_id 
-        ('create_lobby', {'player_id': '2', 'lobby_details': 'my second lobby'}, server_url),                                #player 2 tries to create another lobby
+        (
+            "create_lobby",
+            {"lobby_details": "Example Lobby Details"},
+            server_url,
+        ),  # missing player_id
+        ("create_lobby", {"player_id": "1"}, server_url),  # missing lobby_details
+        (
+            "create_lobby",
+            {"player_id": "100", "lobby_details": "Example Lobby Details"},
+            server_url,
+        ),  # invalid player_id
+        (
+            "create_lobby",
+            {"player_id": "2", "lobby_details": "my second lobby"},
+            server_url,
+        ),  # player 2 tries to create another lobby
     ]
     run_tests(invalid_create_actions)
-    print("\n" + " Testing invalid create lobby cases complete ".center(80, '-')  + "\n")
+    print("\n" + " Testing invalid create lobby cases complete ".center(80, "-") + "\n")
 
 
 
-    #4. Testing invalid join lobbies
-    print("\n" + " Testing invalid join lobby ".center(80, '-'))
+
+    # 4. Testing invalid join lobbies
+    print("\n" + " Testing invalid join lobby ".center(80, "-"))
     invalid_join_actions = [
-        ('join_lobby', {'lobby_id': '1'}, server_url),                                                                      # missing player_id 
-        ('join_lobby', {'player_id': '1'}, server_url),                                                                    # missing lobby_id
-        ('join_lobby', {'player_id': '100', 'lobby_id': '2'}, server_url),                                                 # invalid player_id
-        ('join_lobby', {'player_id': '1', 'lobby_id': '100'}, server_url),                                                 # invalid lobby_id 
-        ('join_lobby', {'player_id': '2', 'lobby_id': '3'}, server_url),                                                    #player 2 tries to join lobby 3 
+        ("join_lobby", {"lobby_id": "1"}, server_url),  # missing player_id
+        ("join_lobby", {"player_id": "1"}, server_url),  # missing lobby_id
+        (
+            "join_lobby",
+            {"player_id": "100", "lobby_id": "2"},
+            server_url,
+        ),  # invalid player_id
+        (
+            "join_lobby",
+            {"player_id": "1", "lobby_id": "100"},
+            server_url,
+        ),  # invalid lobby_id
+        (
+            "join_lobby",
+            {"player_id": "2", "lobby_id": "3"},
+            server_url,
+        ),  # player 2 tries to join lobby 3
     ]
     run_tests(invalid_join_actions)
-    print("\n" + " Testing invalid join lobby cases complete ".center(80, '-')  + "\n")
+    print("\n" + " Testing invalid join lobby cases complete ".center(80, "-") + "\n")
 
 
 
-    #5. Testing invalid leave lobbies
-    print("\n" + " Testing invalid leave lobby ".center(80, '-'))
+
+    # 5. Testing invalid leave lobbies
+    print("\n" + " Testing invalid leave lobby ".center(80, "-"))
     invalid_leave_actions = [
-        ('leave_lobby', {'lobby_id': '1'}, server_url),                                                                      # missing player_id 
-        ('leave_lobby', {'player_id': '1'}, server_url),                                                                    # missing lobby_id
-        ('leave_lobby', {'player_id': '100', 'lobby_id': '1'}, server_url),                                                 # invalid player_id
-        ('leave_lobby', {'player_id': '1', 'lobby_id': '100'}, server_url),                                                 # invalid lobby_id 
-        ('leave_lobby', {'player_id': '2', 'lobby_id': '3'}, server_url),                                                 # player 2 tries to leave lobby 3
-
+        ("leave_lobby", {"lobby_id": "1"}, server_url),  # missing player_id
+        ("leave_lobby", {"player_id": "1"}, server_url),  # missing lobby_id
+        (
+            "leave_lobby",
+            {"player_id": "100", "lobby_id": "1"},
+            server_url,
+        ),  # invalid player_id
+        (
+            "leave_lobby",
+            {"player_id": "1", "lobby_id": "100"},
+            server_url,
+        ),  # invalid lobby_id
+        (
+            "leave_lobby",
+            {"player_id": "2", "lobby_id": "3"},
+            server_url,
+        ),  # player 2 tries to leave lobby 3
     ]
     run_tests(invalid_leave_actions)
-    print("\n" + " Testing invalid leave lobby cases complete ".center(80, '-')  + "\n")
+    print("\n" + " Testing invalid leave lobby cases complete ".center(80, "-") + "\n")
 
 
-    #6. Testing delete lobby when empty 
-    print("\n" + " Testing delete lobby when empty ".center(80, '-'))
-    delete_actions = [('leave_lobby', {'player_id': '2', 'lobby_id': '2'}, server_url)]                   #player 2 leaves lobby 2, now lobby 2 is empty and deleted
+
+
+    # 6. Testing delete lobby when empty
+    print("\n" + " Testing delete lobby when empty ".center(80, "-"))
+    delete_actions = [
+        ("leave_lobby", {"player_id": "2", "lobby_id": "2"}, server_url)
+    ]  # player 2 leaves lobby 2, now lobby 2 is empty and deleted
     run_tests(delete_actions)
     response = requests.get(f"{server_url}/get_lobbies")
-    print("\nResponse from /get_lobbies:")                                                                 #now our get request shows we only have lobby 3 left
+    print(
+        "\nResponse from /get_lobbies:"
+    )  # now our get request shows we only have lobby 3 left
     print(response.status_code, response.json())
-    print("\n" + " Testing delete lobby when empty complete ".center(80, '-'))
-
-
-
+    print("\n" + " Testing delete lobby when empty complete ".center(80, "-"))
--- server.py
+++ server.py
@@ -1,214 +1,223 @@
 from flask import Flask, request, jsonify
 from flask_socketio import join_room, leave_room, send, SocketIO
 
-# #TODO: 
-# 1. add last few invalid create, join and leave test cases 
-# 2. documentation 
-# 3. requirements file   
+# #TODO:
+# 2. documentation
+# 3. requirements file
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 app.config["SECRET_KEY"] = "secret"
 
-#Data structures to store data  
-lobbies = {} #maps lobbies to the list of players
-players = {} #maps players to the lobby they are in, None if not in lobby
-current_lobby_id = 1 #Sets the lobby_id of new lobbies. Using simple integer for easier testing
+# Data structures to store data
+lobbies = {}  # maps lobbies to the list of players
+players = {}  # maps players to the lobby they are in, None if not in lobby
+current_lobby_id = (
+    1  # Sets the lobby_id of new lobbies. Using simple integer for easier testing
+)
 
 
-#insert 20 valid registered players for dummy data, initially not in lobby
+# insert 20 valid registered players for dummy data, initially not in lobby
 for i in range(20):
     players[str(i)] = None
 
-#maximum number of players per lobby - change if needed
+# maximum number of players per lobby - change if needed
 max_players = 5
 
 
-def start_game(lobby_id): 
-    send({"message": f"Max {max_players} players reached. Game is beginning"}, to=lobby_id)
-    #remove everyone from the lobby 
-    for player_id in lobbies[lobby_id]['players']:
+def start_game(lobby_id):
+    send(
+        {"message": f"Max {max_players} players reached. Game is beginning"},
+        to=lobby_id,
+    )
+    # remove everyone from the lobby
+    for player_id in lobbies[lobby_id]["players"]:
         players[player_id] = None
-    
-    #delete the lobby after starting game
+
+    # delete the lobby after starting game
     del lobbies[lobby_id]
 
-    #make everyone leave the socketio room
-    leave_room(lobby_id) 
+    # make everyone leave the socketio room
+    leave_room(lobby_id)
 
-#socket for create lobby 
-@socketio.on('create_lobby')
+
+# socket for create lobby
+@socketio.on("create_lobby")
 def create_lobby(data):
-    #declare global to modify global variables and not local variables
+    # declare global to modify global variables and not local variables
     global lobbies
     global players
-    global current_lobby_id 
+    global current_lobby_id
 
-    #check if player_id is in data
+    # check if player_id is in data
     if "player_id" not in data:
         send({"error": "Missing required parameter: player_id"}, to=request.sid)
         return
 
-    #check if lobby_details is in data
+    # check if lobby_details is in data
     if "lobby_details" not in data:
         send({"error": "Missing required parameter: lobby_details"}, to=request.sid)
         return
 
-    #extract player_id and lobby_details and ensure correct formatting
-    player_id = str(data['player_id'])
-    lobby_details = str(data['lobby_details'])
+    # extract player_id and lobby_details and ensure correct formatting
+    player_id = str(data["player_id"])
+    lobby_details = str(data["lobby_details"])
 
-    #check if player_id is a valid registered player
-    if player_id not in players: 
+    # check if player_id is a valid registered player
+    if player_id not in players:
         send({"error": "Invalid player_id"}, to=request.sid)
         return
-    
-    #check if player_id is already in a lobby 
-    if players[player_id]: 
-        send({"error": f"Player {player_id} is already in lobby {players[player_id]}"}, to=request.sid)
+
+    # check if player_id is already in a lobby
+    if players[player_id]:
+        send(
+            {"error": f"Player {player_id} is already in lobby {players[player_id]}"},
+            to=request.sid,
+        )
         return
 
-    #create a new lobby with current_lobby_id
+    # create a new lobby with current_lobby_id
     lobby_id = str(current_lobby_id)
     lobbies[lobby_id] = {
-        "players": [player_id], #initialize list of players with player_id who created it
+        "players": [
+            player_id
+        ],  # initialize list of players with player_id who created it
         "lobby_details": lobby_details,
     }
 
-    #associate player with lobby
+    # associate player with lobby
     players[player_id] = lobby_id
 
-    #increment current_lobby_id for the next lobby
+    # increment current_lobby_id for the next lobby
     current_lobby_id += 1
 
-    #join the socketio room associated with lobby_id
+    # join the socketio room associated with lobby_id
     join_room(lobby_id)
 
-    #send message that player_id has created a lobby to lobby_id 
+    # send message that player_id has created a lobby to lobby_id
     send({"message": f"Player {player_id} has created lobby {lobby_id}"}, to=lobby_id)
 
-    #return information to the client #modify this based on frontend's needs
+    # return information to the client #modify this based on frontend's needs
     return lobbies[lobby_id]
 
 
-#socket for joining lobby
-@socketio.on('join_lobby')
+# socket for joining lobby
+@socketio.on("join_lobby")
 def join_lobby(data):
-    #declare global to modify global variables and not local variables
+    # declare global to modify global variables and not local variables
     global lobbies
     global players
 
-
-    #check if player_id is in data
+    # check if player_id is in data
     if "player_id" not in data:
         send({"error": "Missing required parameter: player_id"}, to=request.sid)
         return
 
-    #check if lobby_id is in data
+    # check if lobby_id is in data
     if "lobby_id" not in data:
         send({"error": "Missing required parameter: lobby_id"}, to=request.sid)
         return
-    
-    #extract player_id and lobby_id and ensure correct formatting
-    player_id = str(data['player_id'])
-    lobby_id = str(data['lobby_id'])
 
-    #check if player_id is a valid registered player
-    if player_id not in players: 
+    # extract player_id and lobby_id and ensure correct formatting
+    player_id = str(data["player_id"])
+    lobby_id = str(data["lobby_id"])
+
+    # check if player_id is a valid registered player
+    if player_id not in players:
         send({"error": "Invalid player_id"}, to=request.sid)
         return
-    
-    #check if lobby_id is a valid existing lobby
+
+    # check if lobby_id is a valid existing lobby
     if lobby_id not in lobbies:
         send({"error": "Invalid lobby_id, does not exist"}, to=request.sid)
         return
 
-    #check if player_id is already in a lobby 
-    if players[player_id]: 
-        send({"error": f"Player {player_id} is already in lobby {players[player_id]}"}, to=request.sid)
+    # check if player_id is already in a lobby
+    if players[player_id]:
+        send(
+            {"error": f"Player {player_id} is already in lobby {players[player_id]}"},
+            to=request.sid,
+        )
         return
 
-    #join the socketio room associated with lobby_id
+    # join the socketio room associated with lobby_id
     join_room(lobby_id)
 
-    #send message that player_id has joined lobby_id to lobby_id 
+    # send message that player_id has joined lobby_id to lobby_id
     send({"message": f"Player {player_id} has entered the lobby"}, to=lobby_id)
 
-    #modify lobbies and players to include the new player_id
-    lobbies[lobby_id]['players'].append(player_id)
+    # modify lobbies and players to include the new player_id
+    lobbies[lobby_id]["players"].append(player_id)
     players[player_id] = lobby_id
 
-    #check if number of players exceeds max players and start game if True
-    if len(lobbies[lobby_id]['players']) >= max_players:
+    # check if number of players exceeds max players and start game if True
+    if len(lobbies[lobby_id]["players"]) >= max_players:
         start_game(lobby_id)
-        return 
-    
-    #return information to the client #modify this based on frontend's needs
+        return
+
+    # return information to the client #modify this based on frontend's needs
     return lobbies[lobby_id]
 
-#leaving lobby
+
+# leaving lobby
 @socketio.on("leave_lobby")
 def leave_lobby(data):
-    #declare global to modify global variables and not local variables
+    # declare global to modify global variables and not local variables
     global lobbies
     global players
 
-    #check if player_id is in data
+    # check if player_id is in data
     if "player_id" not in data:
         send({"error": "Missing required parameter: player_id"}, to=request.sid)
         return
 
-    #check if lobby_id is in data
+    # check if lobby_id is in data
     if "lobby_id" not in data:
         send({"error": "Missing required parameter: lobby_id"}, to=request.sid)
         return
-    
-    #extract player_id and lobby_id and ensure correct formatting
-    player_id = str(data['player_id'])
-    lobby_id = str(data['lobby_id'])
 
-    #check if player_id is a valid registered player
-    if player_id not in players: 
+    # extract player_id and lobby_id and ensure correct formatting
+    player_id = str(data["player_id"])
+    lobby_id = str(data["lobby_id"])
+
+    # check if player_id is a valid registered player
+    if player_id not in players:
         send({"error": "Invalid player_id"}, to=request.sid)
         return
-    
-    #check if lobby_id is a valid existing lobby
+
+    # check if lobby_id is a valid existing lobby
     if lobby_id not in lobbies:
         send({"error": "Invalid lobby_id, does not exist"}, to=request.sid)
         return
 
-    #check if player_id is not in this lobby
-    if players[player_id] != lobby_id: 
-        
-        send({"error": f"Player {player_id} is not in lobby {lobby_id}"}, to=request.sid)
+    # check if player_id is not in this lobby
+    if players[player_id] != lobby_id:
+
+        send(
+            {"error": f"Player {player_id} is not in lobby {lobby_id}"}, to=request.sid
+        )
         return
 
-    #send message that player_id has left lobby_id to lobby_id 
+    # send message that player_id has left lobby_id to lobby_id
     send({"message": f"Player {player_id} has left the lobby"}, to=lobby_id)
 
-    #make client leave the socketio room associated with lobby_id
+    # make client leave the socketio room associated with lobby_id
     leave_room(lobby_id)
 
-    #modify lobbies and players to remove the player_id
-    lobbies[lobby_id]['players'].remove(player_id)
-    players[player_id] = None 
+    # modify lobbies and players to remove the player_id
+    lobbies[lobby_id]["players"].remove(player_id)
+    players[player_id] = None
 
-    #delete the lobby if there are no more players
-    if len(lobbies[lobby_id]['players']) <= 0:
+    # delete the lobby if there are no more players
+    if len(lobbies[lobby_id]["players"]) <= 0:
         del lobbies[lobby_id]
-    
 
-#list all lobbies
-@app.route('/get_lobbies', methods=['GET'])
+
+# list all lobbies
+@app.route("/get_lobbies", methods=["GET"])
 def get_lobbies():
     return jsonify(lobbies), 200
 
 
-
 if __name__ == "__main__":
     socketio.run(app, debug=True)
-
-
-
-
-
